1d46ce6305ef5c779f93b6ae3a72ba9cb4b73a36
vipul <letsmailvipul@gmail.com>
2018-05-14 20:51:43 +0530

    find shortest paths from source to all vertices in the given graph

--- Algorithm and Data Strucutre/Greedy Algorithm/Dijkstras_SP.py
+++ Algorithm and Data Strucutre/Greedy Algorithm/Dijkstras_SP.py
@@ -35,4 +35,20 @@ class Graph():
 		for count in range(self.V):
 			u = self.minDistance(dist, sptSet)
 			sptSet[u] = True
-			
+			for v in range(self.V):
+				if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]:
+					dist[v] = dist[u] + self.graph[u][v]
+		self.printSolution(dist)
+
+g = Graph(9)
+g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+           [4, 0, 8, 0, 0, 0, 0, 11, 0],
+           [0, 8, 0, 7, 0, 4, 0, 0, 2],
+           [0, 0, 7, 0, 9, 14, 0, 0, 0],
+           [0, 0, 0, 9, 0, 10, 0, 0, 0],
+           [0, 0, 4, 14, 10, 0, 2, 0, 0],
+           [0, 0, 0, 0, 0, 2, 0, 1, 6],
+           [8, 11, 0, 0, 0, 0, 1, 0, 7],
+           [0, 0, 2, 0, 0, 0, 6, 7, 0]
+          ];
+g.dijkstra(0);

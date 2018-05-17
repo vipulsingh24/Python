289f4f7f04bf88a79939b26170b0b6d980f52f87
vipul <letsmailvipul@gmail.com>
2018-05-17 21:20:45 +0530

    .

--- Algorithm and Data Strucutre/Greedy Algorithm/Min_Platform.py
+++ Algorithm and Data Strucutre/Greedy Algorithm/Min_Platform.py
@@ -3,8 +3,28 @@ Implementation of Minimum Platform required
 for a Railway/Bus Station
 '''
 
-
-
+def findPlatform(arr, dep, n):
+	arr.sort()
+	dep.sort()
+	
+	plat_needed = 1
+	result = 1
+	i = 1
+	j = 0
+	
+	while (i < n and j < n):
+		if (arr[i] < dep[j]):
+			plat_needed += 1
+			i += 1
+			
+			if (plat_needed > result):
+				result = plat_needed
+				
+		else:
+			plat_needed -= 1
+			j += 1
+		
+	return result
 
 arr = [900, 940, 950, 1100, 1500, 1800]
 dep = [910, 1200, 1120, 1130, 1900, 2000]

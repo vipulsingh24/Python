efa6517e84c3d3862a33e7e07975402e5c1e2e58
vipul <letsmailvipul@gmail.com>
2018-05-17 21:54:37 +0530

    .

--- Algorithm and Data Strucutre/Greedy Algorithm/Min_Coins.py
+++ Algorithm and Data Strucutre/Greedy Algorithm/Min_Coins.py
@@ -3,7 +3,17 @@ Implementation of Minimum number of coins
 needed to make a change
 '''
 
-change = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+def findMin(n):
+	result = []
+	for i in range(len(change)-1, -1, -1):
+		while(n >= change[i]):
+			n -= change[i]
+			result.append(change[i])
+	
+	return result
+
+
+change = [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
 
 n = int(input('Enter value: '))
-print('Following is minimal number of change for ' 
+print('Following is minimal number of change for ', n, 'is ', findMin(n))

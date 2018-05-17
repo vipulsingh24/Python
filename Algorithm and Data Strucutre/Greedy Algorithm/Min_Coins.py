d413fdab99743802f4a8f7da2dcebb27557c91b5
vipul <letsmailvipul@gmail.com>
2018-05-17 21:41:16 +0530

    Gven a value V find the minimum number of coins required to make a change.

---
+++ Algorithm and Data Strucutre/Greedy Algorithm/Min_Coins.py
@@ -0,0 +1,9 @@
+'''
+Implementation of Minimum number of coins
+needed to make a change
+'''
+
+change = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+
+n = int(input('Enter value: '))
+print('Following is minimal number of change for ' 

9ece04a97db72cf3ae1361685c2770c36cf842c9
vipul <letsmailvipul@gmail.com>
2018-05-21 20:41:14 +0530

    .

--- Algorithm and Data Strucutre/Dynamic Programming/fibonacci.py
+++ Algorithm and Data Strucutre/Dynamic Programming/fibonacci.py
@@ -2,6 +2,7 @@
 Implementation of Fibonacci Serires
 '''
 
+'''
 def fibo(n, f):
 	f[1] = 1
 	
@@ -18,15 +19,24 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
+'''
 
 # Memoized version of nth Fibonacci number
+
+def fibo_memoized(n, lookup):
+	if n == 0 or n == 1:
+		lookup[n] = 1
+	
+	if lookup[n] is None:
+		lookup[n] = fibo_memoized(n-1, lookup) + fibo_memoized(n-2, lookup)
+	
+	return lookup[n]
 
 
 def main():
 	n = int(input('Enter nth term number: '))
 	lookup = [None] * (n+1)
-	print('Fibonacci series upto ', n, 'term is ', fibo(n, lookup))
+	print('Fibonacci series upto ', n, 'term is ', fibo_memoized(n, lookup))
 
-if __name__ == "_main__":
+if __name__ == "__main__":
 	main()

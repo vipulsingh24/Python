c6faff9e6e2cc725b0b17fe389f06e7ecb91fe97
vipul <letsmailvipul@gmail.com>
2018-05-21 20:20:26 +0530

    .

--- Algorithm and Data Strucutre/Dynamic Programming/fibonacci.py
+++ Algorithm and Data Strucutre/Dynamic Programming/fibonacci.py
@@ -2,18 +2,18 @@
 Implementation of Fibonacci Serires
 '''
 
-def fibo(n):
-	f = [0] * (n+1)
+def fibo(n, f):
 	f[1] = 1
 	
 	for i in range(2, n+1):
 		f[i] = f[i-1] + f[i-2]
-	return f[n]
+	return f
 
 
 def main():
 	n = int(input('Enter nth term number: '))
-	print('Fibonacci number is ', fibo(n))
+	f = [0] * (n+1)
+	print('Fibonacci series upto ', n,'term is ', fibo(n, f))
 
 
 if __name__ == "__main__":

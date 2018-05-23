e98a4d7ab5a19170c61f62cb6c7f27a59388db8f
vipul <letsmailvipul@gmail.com>
2018-05-23 21:42:17 +0530

    Longest Increasing Subsequence Problem

--- Algorithm and Data Strucutre/Dynamic Programming/LIS.py
+++ Algorithm and Data Strucutre/Dynamic Programming/LIS.py
@@ -10,13 +10,8 @@ def lis(a):
 		for j in range(0, i):
 			if a[i] > a[j] and lis[i] < lis[j] + 1:
 				lis[i] = lis[j] + 1
-	maximum = 0
-	
-	# Select maximum of all LIS value
-	for i in range(n):
-		maximum = max(maximum, lis[i])
-	
-	return maximum
+
+	return max(lis)
 
 
 a = [10, 22, 9, 33, 21, 50, 41, 60]
